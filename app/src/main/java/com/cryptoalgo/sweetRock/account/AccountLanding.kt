d2ca5393474ccbfe0fd47176a954342750702b08
cryptoAlgorithm <cryptoalgro@gmail.com>
2023-02-19 00:09:10 +0800

    feat (auth): add google sign in

--- app/src/main/java/com/cryptoalgo/sweetRock/account/AccountLanding.kt
+++ app/src/main/java/com/cryptoalgo/sweetRock/account/AccountLanding.kt
@@ -1,18 +1,30 @@
 package com.cryptoalgo.sweetRock.account
 
+import android.app.Activity
 import android.util.Log
+import androidx.activity.compose.rememberLauncherForActivityResult
+import androidx.activity.result.contract.ActivityResultContracts
 import androidx.compose.foundation.Image
+import androidx.compose.foundation.background
 import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.PaddingValues
+import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.size
 import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.material3.Button
 import androidx.compose.material3.CardDefaults
+import androidx.compose.material3.CircularProgressIndicator
+import androidx.compose.material3.Divider
 import androidx.compose.material3.ElevatedCard
 import androidx.compose.material3.ExperimentalMaterial3Api
+import androidx.compose.material3.FilledTonalButton
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.OutlinedTextField
 import androidx.compose.material3.Text
@@ -23,38 +35,86 @@ import androidx.compose.runtime.remember
 import androidx.compose.runtime.rememberCoroutineScope
 import androidx.compose.runtime.saveable.rememberSaveable
 import androidx.compose.runtime.setValue
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Brush
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.graphics.StrokeCap
 import androidx.compose.ui.layout.ContentScale
+import androidx.compose.ui.platform.LocalContext
 import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.text.input.PasswordVisualTransformation
 import androidx.compose.ui.text.input.TextFieldValue
 import androidx.compose.ui.text.style.TextAlign
 import androidx.compose.ui.unit.dp
 import androidx.lifecycle.viewmodel.compose.viewModel
 import com.cryptoalgo.sweetRock.R
+import com.google.android.gms.auth.api.identity.Identity
 import kotlinx.coroutines.launch
+
+private const val TAG = "Account"
 
 @Composable
 fun AccountLanding(
     model: AccountViewModel = viewModel()
 ) {
-    LazyColumn(Modifier.fillMaxSize(), contentPadding = PaddingValues(16.dp)) {
-        item {
+    Box {
+        Box {
             Image(
                 painter = painterResource(id = R.drawable.banner), contentDescription = null,
-                Modifier.fillMaxWidth(),
-                contentScale = ContentScale.FillWidth
+                Modifier
+                    .height(200.dp)
+                    .fillMaxWidth(),
+                contentScale = ContentScale.Crop
+            )
+            Spacer(
+                Modifier
+                    .matchParentSize()
+                    .background(
+                        Brush.verticalGradient(
+                            0.75f to Color.Transparent,
+                            1f to MaterialTheme.colorScheme.background
+                        )
+                    )
             )
         }
-        if (model.user != null) item {
-            ElevatedCard {
-                Text(text = "Logged in as ${model.user?.email}")
-                Button(onClick = { model.signOut() }) {
-                    Text("Sign Out")
+        LazyColumn(Modifier.fillMaxSize(), contentPadding = PaddingValues(16.dp)) {
+            item { Spacer(Modifier.height(140.dp)) }
+            item {
+                when {
+                    model.user != null -> SignedInLanding()
+                    else -> SignInLaunchpad()
                 }
             }
         }
-        else item { SignInLaunchpad() }
+    }
+}
+
+@Composable
+private fun SignedInLanding(
+    model: AccountViewModel = viewModel()
+) {
+    val user = model.user ?: return // Return early if the current user is null
+
+    ElevatedCard(
+        Modifier.fillMaxWidth(),
+        elevation = CardDefaults.elevatedCardElevation(defaultElevation = 0.dp),
+        shape = MaterialTheme.shapes.large
+    ) {
+        Column(Modifier.padding(16.dp, 12.dp), verticalArrangement = Arrangement.spacedBy(4.dp)) {
+            Text("Your Account", fontWeight = FontWeight.SemiBold, style = MaterialTheme.typography.headlineMedium)
+            user.displayName?.let {
+                Text(it)
+            }
+            Text(user.email ?: "No Email")
+            Button(onClick = { model.signOut() },
+                Modifier
+                    .fillMaxWidth()
+                    .padding(top = 8.dp)) {
+                Text("Sign Out")
+            }
+        }
     }
 }
 
@@ -64,8 +124,44 @@ private fun SignInLaunchpad(
     model: AccountViewModel = viewModel()
 ) {
     val coroutineScope = rememberCoroutineScope()
+    val context = LocalContext.current
 
+    var loadingMethod by remember { mutableStateOf<Int?>(null) }
     var error by remember { mutableStateOf<String?>(null) }
+
+    val launcher = rememberLauncherForActivityResult(
+        ActivityResultContracts.StartIntentSenderForResult()
+    ) { result ->
+        if (result.resultCode != Activity.RESULT_OK) {
+            loadingMethod = null
+            // The user cancelled the login, was it due to an Exception?
+            if (result.data?.action == ActivityResultContracts.StartIntentSenderForResult.ACTION_INTENT_SENDER_REQUEST) {
+                /*val exception: Exception? = result.data?.getSerializableExtra(
+                    ActivityResultContracts.StartIntentSenderForResult.EXTRA_SEND_INTENT_EXCEPTION)*/
+                Log.e(TAG, "Couldn't start One Tap UI")
+            } else Log.w(TAG, "User cancelled flow")
+            return@rememberLauncherForActivityResult
+        }
+        val oneTapClient = Identity.getSignInClient(context)
+        val credential = oneTapClient.getSignInCredentialFromIntent(result.data)
+        val idToken = credential.googleIdToken
+        if (idToken != null) {
+            // Got an ID token from Google. Use it to authenticate
+            // with your backend.
+            coroutineScope.launch {
+                try {
+                    model.signIn(idToken)
+                } catch (e: Exception) {
+
+                } finally {
+                    loadingMethod = null
+                }
+            }
+        } else {
+            Log.w("LOG", "Null Token")
+            loadingMethod = null
+        }
+    }
 
     var email by rememberSaveable(stateSaver = TextFieldValue.Saver) {
         mutableStateOf(TextFieldValue(""))
@@ -75,22 +171,28 @@ private fun SignInLaunchpad(
     }
 
     ElevatedCard(
-        shape = MaterialTheme.shapes.extraLarge,
-        elevation = CardDefaults.elevatedCardElevation(defaultElevation = 3.dp)
+        elevation = CardDefaults.elevatedCardElevation(defaultElevation = 0.dp),
+        shape = MaterialTheme.shapes.large
     ) {
-        Column(Modifier.padding(12.dp, 8.dp), verticalArrangement = Arrangement.spacedBy(8.dp)) {
+        Column(Modifier.padding(8.dp, 8.dp), verticalArrangement = Arrangement.spacedBy(8.dp)) {
             Text(
-                text = "Welcome Back",
+                "Welcome Back",
                 Modifier
                     .fillMaxWidth()
-                    .padding(top = 8.dp), textAlign = TextAlign.Center,
+                    .padding(top = 8.dp),
+                fontWeight = FontWeight.SemiBold,
+                textAlign = TextAlign.Center,
                 style = MaterialTheme.typography.headlineMedium
             )
             Text(
-                text = "Save and sync your cart, place orders, add reviews and more!",
-                Modifier.fillMaxWidth(), textAlign = TextAlign.Center,
+                text = "Place orders, add reviews and more!",
+                Modifier
+                    .fillMaxWidth()
+                    .padding(bottom = 4.dp), textAlign = TextAlign.Center,
                 style = MaterialTheme.typography.bodySmall
             )
+
+            // Email sign-in
             OutlinedTextField(
                 value = email, onValueChange = { email = it },
                 Modifier.fillMaxWidth(),
@@ -107,19 +209,59 @@ private fun SignInLaunchpad(
 
             Button(
                 onClick = { coroutineScope.launch {
+                    loadingMethod = 0
                     try {
                         model.signIn(email.text, password.text)
                     } catch (e: Exception) {
                         error = e.message
                         Log.d("AccountLanding", "Sign in failed")
+                    } finally {
+                        loadingMethod = null
                     }
                 }},
-                Modifier
-                    .fillMaxWidth()
-                    .padding(top = 4.dp),
-                enabled = email.text.trim().isNotBlank() && password.text.isNotEmpty()
+                Modifier.fillMaxWidth(),
+                enabled = email.text.trim().isNotBlank() && password.text.isNotEmpty() && loadingMethod != 0
             ) {
-                Text("Log in")
+                if (loadingMethod == 0) {
+                    CircularProgressIndicator(
+                        Modifier.padding(end = 8.dp).size(16.dp),
+                        strokeWidth = 2.dp,
+                        strokeCap = StrokeCap.Round
+                    )
+                }
+                Text("Continue with Email")
+            }
+
+            Row(
+                Modifier.padding(vertical = 4.dp),
+                horizontalArrangement = Arrangement.spacedBy(8.dp),
+                verticalAlignment = Alignment.CenterVertically
+            ) {
+                Divider(Modifier.weight(1f))
+                Text("You can also...", style = MaterialTheme.typography.titleSmall)
+                Divider(Modifier.weight(1f))
+            }
+
+            FilledTonalButton(
+                onClick = { coroutineScope.launch {
+                    loadingMethod = 1
+                    try {
+                        model.signIn(context, launcher)
+                    } catch (e: Exception) {
+                        // Problem initiating sign in flow
+                        loadingMethod = null
+                    }
+                }},
+                Modifier.fillMaxWidth()
+            ) {
+                if (loadingMethod == 1) {
+                    CircularProgressIndicator(
+                        Modifier.padding(end = 8.dp).size(16.dp),
+                        strokeWidth = 2.dp,
+                        strokeCap = StrokeCap.Round
+                    )
+                }
+                Text("Continue with Google")
             }
         }
     }
--- app/src/main/java/com/cryptoalgo/sweetRock/account/AccountViewModel.kt
+++ app/src/main/java/com/cryptoalgo/sweetRock/account/AccountViewModel.kt
@@ -1,12 +1,20 @@
 package com.cryptoalgo.sweetRock.account
 
+import android.content.Context
 import android.util.Log
+import androidx.activity.result.ActivityResultLauncher
+import androidx.activity.result.IntentSenderRequest
 import androidx.compose.runtime.getValue
 import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.setValue
 import androidx.lifecycle.ViewModel
+import com.cryptoalgo.sweetRock.R
+import com.google.android.gms.auth.api.identity.BeginSignInRequest
+import com.google.android.gms.auth.api.identity.Identity
+import com.google.firebase.auth.GoogleAuthProvider
 import com.google.firebase.auth.ktx.auth
 import com.google.firebase.ktx.Firebase
+import kotlinx.coroutines.tasks.await
 import java.lang.Error
 import kotlin.coroutines.resume
 import kotlin.coroutines.resumeWithException
@@ -33,6 +41,9 @@ class AccountViewModel: ViewModel() {
         }
     }
 
+    /**
+     * Sign in with email and password
+     */
     suspend fun signIn(email: String, password: String) = suspendCoroutine<Unit> {
         if (user != null) throw AuthErrors("Already signed in")
 
@@ -43,6 +54,62 @@ class AccountViewModel: ViewModel() {
                     it.resume(Unit)
                 } else {
                     Log.d(TAG, "Sign in failure: ${task.exception}")
+                    it.resumeWithException(task.exception!!)
+                }
+            }
+    }
+
+    /**
+     * Starts the sign in flow with Google one tap sign in
+     */
+    @Throws
+    suspend fun signIn(
+        context: Context,
+        launcher: ActivityResultLauncher<IntentSenderRequest>
+    ) {
+        val oneTapClient = Identity.getSignInClient(context)
+        val signInRequest = BeginSignInRequest.builder()
+            .setGoogleIdTokenRequestOptions(
+                BeginSignInRequest.GoogleIdTokenRequestOptions.builder()
+                    .setSupported(true)
+                    .setFilterByAuthorizedAccounts(false)
+                    // Your server's client ID, not your Android client ID.
+                    .setServerClientId(context.getString(R.string.server_client_id))
+                    // Only show accounts previously used to sign in.
+                    .build()
+            )
+            // Automatically sign in when exactly one credential is retrieved.
+            // .setAutoSelectEnabled(true)
+            .build()
+        try {
+            // Use await() from https://kotlin.github.io/kotlinx.coroutines/kotlinx-coroutines-play-services
+            // Instead of listeners that aren't cleaned up automatically
+            val result = oneTapClient.beginSignIn(signInRequest).await()
+
+            // Now construct the IntentSenderRequest the launcher requires
+            val intentSenderRequest = IntentSenderRequest.Builder(result.pendingIntent).build()
+            launcher.launch(intentSenderRequest)
+        } catch (e: Exception) {
+            // No saved credentials found. Launch the One Tap sign-up flow, or
+            // do nothing and continue presenting the signed-out UI.
+            Log.d(TAG, "Cannot start sign in flow: ${e.message}")
+            // Problem starting sign up flow
+            throw e
+        }
+    }
+
+    /**
+     * Complete Google sign in flow with provided ID token
+     */
+    suspend fun signIn(idToken: String) = suspendCoroutine<Unit> {
+        Log.d(TAG,  "Signing in with ID token")
+        auth.signInWithCredential(GoogleAuthProvider.getCredential(idToken, null))
+            .addOnCompleteListener { task ->
+                if (task.isSuccessful) {
+                    Log.d(TAG, "Signed in successfully with Google credentials")
+                    it.resume(Unit)
+                } else {
+                    Log.w(TAG, "Google credential sign in problem: ${task.exception}")
                     it.resumeWithException(task.exception!!)
                 }
             }
