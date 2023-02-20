cbc424cc0a052b69e2e4ace148a0d724d09cd6fc
cryptoAlgorithm <cryptoalgro@gmail.com>
2023-02-20 20:33:43 +0800

    feat (about): add contact info

--- app/src/main/java/com/cryptoalgo/sweetRock/About.kt
+++ app/src/main/java/com/cryptoalgo/sweetRock/About.kt
@@ -1,18 +1,28 @@
 package com.cryptoalgo.sweetRock
 
+import android.content.Intent
+import android.net.Uri
 import androidx.compose.foundation.Image
 import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.size
 import androidx.compose.foundation.layout.width
 import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.material.icons.Icons
 import androidx.compose.material.icons.rounded.ArrowBack
+import androidx.compose.material.icons.rounded.Email
+import androidx.compose.material.icons.rounded.Phone
 import androidx.compose.material3.CenterAlignedTopAppBar
 import androidx.compose.material3.ExperimentalMaterial3Api
 import androidx.compose.material3.Icon
 import androidx.compose.material3.IconButton
 import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.OutlinedCard
 import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Text
 import androidx.compose.material3.TopAppBarDefaults
@@ -22,16 +32,20 @@ import androidx.compose.ui.Modifier
 import androidx.compose.ui.draw.clip
 import androidx.compose.ui.input.nestedscroll.nestedScroll
 import androidx.compose.ui.layout.ContentScale
+import androidx.compose.ui.platform.LocalContext
 import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.text.style.TextAlign
 import androidx.compose.ui.unit.dp
+import androidx.core.content.ContextCompat.startActivity
+
 
 @OptIn(ExperimentalMaterial3Api::class)
 @Composable
 fun About(onBack: () -> Unit) {
     val scrollBehavior = TopAppBarDefaults.pinnedScrollBehavior()
+    val context = LocalContext.current
 
     Scaffold(
         Modifier.nestedScroll(scrollBehavior.nestedScrollConnection),
@@ -76,6 +90,46 @@ fun About(onBack: () -> Unit) {
                     textAlign = TextAlign.Center,
                     style = MaterialTheme.typography.bodyLarge
                 )
+            }
+            item {
+                OutlinedCard(Modifier.padding(16.dp, 8.dp)) {
+                    Column(
+                        Modifier
+                            .fillMaxWidth()
+                            .padding(16.dp, 12.dp),
+                    ) {
+                        Text("Contact", Modifier.padding(bottom = 4.dp), style = MaterialTheme.typography.titleLarge)
+                        Row(verticalAlignment = Alignment.CenterVertically) {
+                            Text("Email: info@srock.com")
+                            Spacer(Modifier.weight(1f))
+                            IconButton(onClick = {
+                                val intent = Intent(Intent.ACTION_SENDTO).apply {
+                                    data = Uri.parse("mailto:")
+                                    putExtra(Intent.EXTRA_EMAIL, arrayOf("info@srock.com"))
+                                    putExtra(Intent.EXTRA_SUBJECT, "Re: Sweet Rock")
+                                }
+                                try {
+                                    startActivity(context, intent, null)
+                                } catch (_: Throwable) {}
+                            }, Modifier.size(36.dp)) {
+                                Icon(Icons.Rounded.Email, contentDescription = "Open email client")
+                            }
+                        }
+                        Row(verticalAlignment = Alignment.CenterVertically) {
+                            Text("Phone: +65 8124 0500")
+                            Spacer(Modifier.weight(1f))
+                            IconButton(onClick = {
+                                startActivity(
+                                    context,
+                                    Intent(Intent.ACTION_DIAL).apply { data = Uri.parse("tel:+6581240500") },
+                                    null
+                                )
+                            }, Modifier.size(36.dp)) {
+                                Icon(Icons.Rounded.Phone, contentDescription = "Open dialer")
+                            }
+                        }
+                    }
+                }
             }
         }
     }
