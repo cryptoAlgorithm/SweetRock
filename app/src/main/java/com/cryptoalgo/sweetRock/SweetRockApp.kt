ffe6ff8c80146c8b9edb911e775d252a98435ede
cryptoAlgorithm <cryptoalgro@gmail.com>
2023-02-20 20:52:02 +0800

    fix: temporarily disable app check

--- app/src/main/java/com/cryptoalgo/sweetRock/SweetRockApp.kt
+++ app/src/main/java/com/cryptoalgo/sweetRock/SweetRockApp.kt
@@ -1,11 +1,7 @@
 package com.cryptoalgo.sweetRock
 
 import android.app.Application
-import android.content.pm.ApplicationInfo
 import com.google.firebase.FirebaseApp
-import com.google.firebase.appcheck.FirebaseAppCheck
-import com.google.firebase.appcheck.debug.DebugAppCheckProviderFactory
-import com.google.firebase.appcheck.playintegrity.PlayIntegrityAppCheckProviderFactory
 
 class SweetRockApp: Application() {
     override fun onCreate() {
@@ -13,10 +9,11 @@ class SweetRockApp: Application() {
 
         // Initialise Firebase App Check
         FirebaseApp.initializeApp(this)
-        val appCheck = FirebaseAppCheck.getInstance()
-        appCheck.installAppCheckProviderFactory(
-            if (0 != applicationInfo.flags and ApplicationInfo.FLAG_DEBUGGABLE) DebugAppCheckProviderFactory.getInstance()
-            else PlayIntegrityAppCheckProviderFactory.getInstance()
-        )
+        // Temporarily disabled app check
+        // val appCheck = FirebaseAppCheck.getInstance()
+        /*appCheck.installAppCheckProviderFactory(
+            // if (0 != (applicationInfo.flags and ApplicationInfo.FLAG_DEBUGGABLE)) DebugAppCheckProviderFactory.getInstance()
+            PlayIntegrityAppCheckProviderFactory.getInstance()
+        )*/
     }
 }
